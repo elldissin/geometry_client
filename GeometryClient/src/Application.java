86c6901f4fa5a06f82085f90863284e2ed0d59b7
elldissin <elldissin@gmail.com>
2016-09-13 21:12:55 +0300

    Moved GUI to EDT

--- GeometryClient/src/Application.java
+++ GeometryClient/src/Application.java
@@ -1,24 +1,15 @@
-import java.awt.BorderLayout;
-
-import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 import my.games.geometry.game.Client;
+import my.games.geometry.game.ClientWindow;
 
 public class Application {
 	private static final long serialVersionUID = 1L;
 
 	public static void main(String[] args) {
 		Client client = new Client();
-		JFrame frame = new JFrame("Geometry game");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(800, 700);
-		frame.setResizable(false);
-		frame.add(client.getRenderEngine().getCamera(1), BorderLayout.LINE_START);
-		frame.add(client.getRenderEngine().getCamera(2), BorderLayout.LINE_END);
-		frame.add(client.getRenderEngine().getCamera(3), BorderLayout.PAGE_END);
-		frame.setVisible(true);
-		frame.setFocusable(true); // important call to allow listening to keys
-		frame.addKeyListener(client.getController());
+		ClientWindow window = new ClientWindow(client);
+		SwingUtilities.invokeLater(window);
 		client.start();
 	}
 }
