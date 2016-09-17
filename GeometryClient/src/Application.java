7c578b72bc129aa49e953d7a243e0b806d1a0700
qwerty367 <qwerty@hot.ee>
2016-09-17 18:47:31 +0300

    Addded log in window

--- GeometryClient/src/Application.java
+++ GeometryClient/src/Application.java
@@ -2,11 +2,18 @@ import javax.swing.SwingUtilities;
 
 import my.games.geometry.game.Client;
 import my.games.geometry.game.ClientWindow;
+import my.games.geometry.game.LoginWindow;
 
 public class Application {
 	private static final long serialVersionUID = 1L;
 
 	public static void main(String[] args) {
+		LoginWindow loginWindow = new LoginWindow();
+		SwingUtilities.invokeLater(loginWindow);
+		while (loginWindow.isLogButtClicked() == false) {
+			// System.out.println("logButtClicked " +
+			// loginWindow.isLogButtClicked());
+		}
 		Client client = new Client();
 		ClientWindow window = new ClientWindow(client);
 		SwingUtilities.invokeLater(window);
