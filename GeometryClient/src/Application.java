05733542352372fb3d1ad69b1af3c02be9d5b1f0
qwerty367 <Daenisvolkov@gmail.com>
2016-09-25 17:37:02 +0300

    client frame reworked

--- GeometryClient/src/Application.java
+++ GeometryClient/src/Application.java
@@ -2,19 +2,19 @@ import javax.swing.SwingUtilities;
 
 import my.games.geometry.game.Client;
 import my.games.geometry.game.ClientWindow;
-import my.games.geometry.game.LoginWindow;
 
 public class Application {
 	private static final long serialVersionUID = 1L;
 
 	public static void main(String[] args) {
-		LoginWindow loginWindow = new LoginWindow();
-		SwingUtilities.invokeLater(loginWindow);
-		while (loginWindow.isLogButtClicked() == false) {
-		}
+		// LoginWindow loginWindow = new LoginWindow();
+		// SwingUtilities.invokeLater(loginWindow);
+		// while (loginWindow.isLogButtClicked() == false) {
+		// }
 		Client client = new Client();
+		client.start();
 		ClientWindow window = new ClientWindow(client);
 		SwingUtilities.invokeLater(window);
-		client.start();
+
 	}
 }
