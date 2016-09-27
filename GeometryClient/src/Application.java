6be9d610cb9961dd57878d8b0790d06f60e5423a
elldissin <elldissin@gmail.com>
2016-09-27 19:53:19 +0300

    Now client is started by login window

--- GeometryClient/src/Application.java
+++ GeometryClient/src/Application.java
@@ -7,12 +7,7 @@ public class Application {
 	private static final long serialVersionUID = 1L;
 
 	public static void main(String[] args) {
-		// LoginWindow loginWindow = new LoginWindow();
-		// SwingUtilities.invokeLater(loginWindow);
-		// while (loginWindow.isLogButtClicked() == false) {
-		// }
 		Client client = new Client();
-		client.start();
 		ClientWindow window = new ClientWindow(client);
 		SwingUtilities.invokeLater(window);
 
